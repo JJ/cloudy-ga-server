d8793ad6f3debe0b8643afd8fed12d17bf182797
Mario Garcia Valdez <mariosky@gmail.com>
2015-10-07 20:16:45 -0700

    Twix Ajax Library

--- public/js/trapWorker.js
+++ public/js/trapWorker.js
@@ -1,5 +1,5 @@
 importScripts("nodeo.js");
-importScripts("twix.min.js");
+importScripts("twix.js");
 
 self.addEventListener('message', function(e) {
   var data = e.data;
@@ -45,15 +45,11 @@ function init_worker(config) {
     });
     //Worker uuid
     self.uuid = config.worker_uuid;
-    var xmlhttp = new XMLHttpRequest();
-    // PUTs worker info
-    xmlhttp.open("PUT", "/start/"+uuid+"/with/"+population_size, true);
-    xmlhttp.onreadystatechange = function() {
-        if (xmlhttp.readyState == 4 ) {
 
-            if (xmlhttp.status == 200 /*|| xmlhttp.status == 304*/)
-            {
-                var data = JSON.parse(xmlhttp.responseText);
+    Twix.ajax({
+            type:"PUT",
+            url: "/start/"+uuid+"/with/"+population_size,
+            success: function(data) {
                 console.log( data.experiment_id  );
                 if ( data.experiment_id !== "undefined"  ) {
 
@@ -61,27 +57,26 @@ function init_worker(config) {
                     console.log(self.experiment_id);
                 }
 
-                postMessage(
-                    {
+                postMessage({
                         status:'created'
                     });
 
-            }
-            else if (xmlhttp2.readyState == 4 &&  xmlhttp.status == 404)
-            {
-                /// UNABLE TO INITIALIZE
-                console.log("Error 404: not_created");
-                // TO DO: HANDLE
-                postMessage(
-                    {
-                        status:'not_created'
-                    });
+                postMessage(data);
+            },
 
-            }
+            error: function(jgXHR) {
+                if (jgXHR.status ==  404) {
+                    /// UNABLE TO INITIALIZE
+                    console.log("Error 404: not_created");
+                    // TO DO: HANDLE
+                    postMessage({
+                            status:'not_created'
+                        });
 
-        }
-    };
-    xmlhttp.send();
+                }
+            }
+        });
+
 }
 
 
@@ -117,118 +112,90 @@ function do_ea() {
 
 
 
-        Twix.get("/random", function( data ) {
+        Twix.ajax({
+            type:"GET",
+            url: "/random",
+            success: function(data) {
+                if ( data.chromosome ) {
+                    eo.incorporate( data.chromosome );
+                }
+            },
 
-            //ips_data.labels.push(generation_count);
-            //ips_data.datasets[0].data.push( Object.keys( data ).length );
-            //ips_chart.Line(ips_data);
-
+            error: function(jgXHR) {
+                if (jgXHR.status ==  404) {
+                    console.log("404 No Poulation Yet");
+                    //There is no population
+                    postMessage(
+                        {
+                            status:'no_work',
+                            generation_count:eo.generation_count,
+                            best:eo.population[0].string,
+                            fitness:eo.population[0].fitness,'period':period,
+                            pop_size: eo.population.length
+                        });
+                }
+            }
         });
 
-
-        // gets a random chromosome from the pool
-        var xmlhttp = new XMLHttpRequest();
-        var url = "/random";
-        xmlhttp.open("GET", url, true);
-        xmlhttp.onreadystatechange = function() {
-
-
-           if (xmlhttp.readyState == 4 ) {
-
-                if (xmlhttp.status == 200 /*|| xmlhttp.status == 304*/)
-                {
-                    var data = JSON.parse(xmlhttp.responseText);
-                    if ( data.chromosome ) {
-                        eo.incorporate( data.chromosome );
-                       }
-                
-                }
-                else if ( xmlhttp.status == 404)
-                    {   console.log("404");
-                        //There is no population
-                        postMessage( 
-                        { 
-                        status:'no_work',     
-                        generation_count:eo.generation_count, 
-                        best:eo.population[0].string, 
-                        fitness:eo.population[0].fitness,'period':period,
-                        pop_size: eo.population.length
-                        });
-                        console.log("AFTER 404");
-                        //No more work
-                        //return;      
-
-                    }
-
-            }
-        };
-        xmlhttp.send();
-
-
         if (self.experiment_id  !== "undefined" ) {
-            var xmlhttp2 = new XMLHttpRequest();
-            // And puts another one in the pool
-            xmlhttp2.open("PUT", "/experiment/" + self.experiment_id + "/one/" + eo.population[0].string + "/" + eo.population[0].fitness + "/" + uuid, true);
-            xmlhttp2.onreadystatechange = function () {
-                if (xmlhttp2.readyState == 4 && xmlhttp2.status == 200) {
 
+            Twix.ajax({
+                type:"PUT",
+                url:  "/experiment/" + self.experiment_id + "/one/" + eo.population[0].string + "/" + eo.population[0].fitness + "/" + uuid,
+                success: function(data) {
                     postMessage(
                         {
                             status: 'working',
                             generation_count: eo.generation_count,
                             best: eo.population[0].string,
-                            fitness: eo.population[0].fitness, 'period': period, 'ips': ips,
+                            fitness: eo.population [0].fitness,
+                            'period': self.period,
+                            'ips': self.ips,
                             pop_size: eo.population.length,
                             current_expid: self.experiment_id
                         });
-                }
-                else if (xmlhttp2.readyState == 4 && xmlhttp2.status == 410) {
-                    var data = JSON.parse(xmlhttp2.responseText);
-                    self.experiment_id = data.current_expid;
+                },
 
-                    postMessage({
+                error: function(status, text, jgXHR) {
+                    if ( status ==  410) {
+
+                        var data = JSON.parse(jgXHR.responseText);
+
+                        self.experiment_id = data.current_expid;
+
+                        console.log(self.experiment_id );
+
+                        postMessage({
                             status: 'experiment_not_found',
                             current_experiment_id:self.experiment_id
                         });
-                    restart();
+                        restart();
 
-
+                    }
                 }
+            });
 
-
-            };
-            xmlhttp2.send();
         }
         //Workers
-        var xmlhttp3 = new XMLHttpRequest();
-        var url = "/workers";
-        var ips = "";
-        xmlhttp3.onreadystatechange = function() {
-            if (xmlhttp3.readyState == 4 && xmlhttp3.status == 200) {
-                var data = JSON.parse(xmlhttp3.responseText);
-                ips = Object.keys( data ).length;
+
+        Twix.ajax({
+            type:"GET",
+            url: "/workers",
+            success: function(data) {
+                self.ips = Object.keys( data ).length;
 
 
                 postMessage({
                     status:'working',
-                    generation_count:eo.generation_count, 
-                    best:eo.population[0].string, 
+                    generation_count:eo.generation_count,
+                    best:eo.population[0].string,
                     fitness:eo.population[0].fitness,
                     'period':period,
-                    'ips':ips,
+                    'ips':self.ips,
                     pop_size: eo.population.length
-                    });
-
-
-                }
-
-
-            };
-        xmlhttp3.open("GET", url, true);
-        xmlhttp3.send();
-
-
-       
+                });
+            }
+        });
     }
 
     if ( eo.population[0].fitness < traps*trap_b ) {
@@ -243,42 +210,37 @@ function do_ea() {
 
         if (self.experiment_id  !== "undefined" ) {
 
-            var xmlhttp2 = new XMLHttpRequest();
-            ///experiment/:expid/one/:chromosome/:fitness/:uuid
-            xmlhttp2.open("PUT", "/experiment/" + self.experiment_id + "/one/" + eo.population[0].string + "/" + eo.population[0].fitness + "/" + uuid, true);
-            xmlhttp2.onreadystatechange = function () {
-                if (xmlhttp2.readyState == 4 && xmlhttp2.status == 200) {
+            Twix.ajax({
+                type:"PUT",
+                url:  "/experiment/" + self.experiment_id + "/one/" + eo.population[0].string + "/" + eo.population[0].fitness + "/" + uuid,
+                success: function(data) {
                     postMessage(
                         {
-                            status:'finished',
+                            status: 'finished',
                             generation_count: eo.generation_count,
                             best: eo.population[0].string,
-                            fitness: eo.population[0].fitness, 'period': period, 'ips': ips,
+                            fitness: eo.population [0].fitness, 'period': period, 'ips': self.ips,
                             pop_size: eo.population.length,
                             current_expid: self.experiment_id
                         });
+                    restart();
+                },
 
-
-                }
-                else if (xmlhttp2.readyState == 4 && xmlhttp2.status == 410) {
-                    var data = JSON.parse(xmlhttp2.responseText);
-                    self.experiment_id = data.current_expid;
-
-                    postMessage(
-                        {
-                           status: 'experiment_not_found',
-                           current_experiment_id:self.experiment_id
-
+                error: function(status, text, jgXHR) {
+                    if (status ==  410) {
+                        var data = JSON.parse(jgXHR.responseText);
+                        self.experiment_id = data.current_expid;
+                        console.log(self.experiment_id );
+                        postMessage({
+                            status: 'experiment_not_found',
+                            current_experiment_id:self.experiment_id
                         });
+                        restart();
 
-
+                    }
                 }
-                // Restart either way
-                restart();
-                console.log('finished after');
-            };
+            });
 
-            xmlhttp2.send();
         }
 
 
